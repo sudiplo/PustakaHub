5097efef23cf308ab328af46dda1a2ae1a388eb8
sudiplo <leosudip949@gmail.com>
2026-07-17 16:13:04 +0545

    user table modify

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>PustakaHub – Library Management</title>
 
-    <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -15,7 +14,7 @@
             max-height: 0;
             overflow: hidden;
             transition: max-height 0.4s ease-in-out;
-        }
+        }pp
         #mobile-menu.open {
             max-height: 400px;
         }
